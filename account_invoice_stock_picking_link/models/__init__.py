e790f1a4dcd4f0ffa7ba91a8fe2c34276a98647b
Nick Booker <nick.booker@opusvl.com>
2017-01-17 12:38:48 +0000

    Remove dead whitespace

--- account_invoice_stock_picking_link/models/__init__.py
+++ account_invoice_stock_picking_link/models/__init__.py
@@ -21,11 +21,8 @@
 ##############################################################################
 
 from . import (
-
     account,
-
     stock,
-
 )
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
