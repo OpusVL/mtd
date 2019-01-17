4914eb368ee8ce083675c7627b6c60b783a50aae
Shree Sangani <shree.sangani@opusvl.com>
2019-01-17 13:05:59 +0000

    [INI][O1758]- MTD Initial commit
    This commit contains successful completion of Hello world API and hello
    application API. There are some changes which are still required but
    at least we can establish a connection. The hello user step one getting
    the authorisation code successfully achieved (still need to handle the
    exceptions) and step two gaining the access code and refresh code also
    works. Now need to receive the hello user message (step three from the
    connection.)

---
+++ account_mtd/models/mtd_api.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class MtdApiTokens(models.Model):
+    _name = 'mtd.api'
+    _description = "list of api table"
+    
+    name = fields.Char('Name', required=True)
+    scope = fields.Char('Scope', required=True)
---
+++ account_mtd/models/mtd_api_request_tracker.py
@@ -0,0 +1,13 @@
+from odoo import models, fields, api
+
+class MtdApiRequestTracker(models.Model):
+    _name = 'mtd.api_request_tracker'
+    _description = "authorisation request table"
+    
+    user_id = fields.Integer('User Id', required=True)
+    api_id = fields.Integer('API Id', required=True)
+    api_name = fields.Char('API Name', required=True)
+    endpoint_id = fields.Char('Endpoint Id', required=True)
+    request_sent = fields.Boolean('Request Sent', required=True)
+    request_received = fields.Boolean('Request Received')
+    
