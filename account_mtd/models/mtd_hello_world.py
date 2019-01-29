4e0e1e01bc80da3feb416e173549b11e8b586ab2
Shree Sangani <shree.sangani@opusvl.com>
2019-01-29 17:34:39 +0000

    [IMP][O1758] typo and refactoring each if to a single call

--- account_mtd/models/mtd_hello_world.py
+++ account_mtd/models/mtd_hello_world.py
@@ -29,81 +29,92 @@ class MtdHelloWorld(models.Model):
     @api.multi
     def action_hello_world_connection(self):
         if not self.hmrc_configuration:
-            raise exceptions.Warning("Please select hmrc configuration before continuing!")
+            raise exceptions.Warning("Please select HMRC configuration before continuing!")
         # making sure that the endpoint is what we have used in the data file as the name can be changed anytime.
-        endpoint_record = self.env['ir.model.data'].search([('res_id','=',self.id), ('model', '=', 'mtd.hello_world')])
+        endpoint_record = self.env['ir.model.data'].search([('res_id','=', self.id), ('model', '=', 'mtd.hello_world')])
 
         if endpoint_record.name == "mtd_hello_world_endpoint":
-            self.which_button_type_clicked = "helloworld"
-            self.path = "/hello/world"
-            version = self._json_command('version')
-            self._logger.info(
-                "Connection button Clicked - endpoint name {}, ".format(self.name) +
-                "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
-            )
+            return self._handle_mtd_hello_world_endpoint()
         elif endpoint_record.name == "mtd_hello_application_endpoint":
-            self.which_button_type_clicked = "application"
-            self.path = "/hello/application"
-            version = self._json_command('version')
-            self._logger.info(
-                "Connection button Clicked - endpoint name {},".format(self.name) +
-                "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
-            )
+            return self._handle_mtd_hello_application_endpoint()
         elif endpoint_record.name == "mtd_hello_user_endpoint":
-            self.which_button_type_clicked = "user"
-            self.path = "/hello/user"
+            return self._handle_mtd_hello_user_endpoint()
+        else:
+            raise exceptions.Warning("Could not connect to HMRC! \nThis is not a valid HMRC service connection")
+
+    def _handle_mtd_hello_world_endpoint(self):
+        self.which_button_type_clicked = "helloworld"
+        self.path = "/hello/world"
+        version = self._json_command('version')
+        self._logger.info(
+            "Connection button Clicked - endpoint name {}, ".format(self.name) +
+            "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
+        )
+        return version
+
+    def _handle_mtd_hello_application_endpoint(self):
+        self.which_button_type_clicked = "application"
+        self.path = "/hello/application"
+        version = self._json_command('version')
+        self._logger.info(
+            "Connection button Clicked - endpoint name {},".format(self.name) +
+            "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
+        )
+        return version
+
+    def _handle_mtd_hello_user_endpoint(self):
+        self.which_button_type_clicked = "user"
+        self.path = "/hello/user"
+        self._logger.info(
+            "Connection button Clicked - endpoint name {}, ".format(self.name) +
+            "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
+        )
+        # search for token record for the API
+        token_record = self.env['mtd.api_tokens'].search([('api_id', '=', self.api_name.id)])
+        self._logger.info(
+            "Connection button Clicked - endpoint name {} , ".format(self.name) +
+            "and the api is :- {} ".format(self.api_name)
+        )
+        if not token_record.access_token and not token_record.refresh_token:
+            self._logger.info(
+                "Connection button Clicked - endpoint name {0}, No access token ".format(self.name) +
+                "found and no refresh_token found from the token record table."
+            )
+            api_tracker = self.env['mtd.api_request_tracker'].search([
+                ('request_sent', '=', True),
+                ('response_received', '=', False)
+            ])
             self._logger.info(
                 "Connection button Clicked - endpoint name {}, ".format(self.name) +
-                "redirect URL:- {}, Path url:- {}".format(self.hmrc_configuration.redirect_url, self.path)
+                "Checking to see if a request is in process"
             )
-            # search for token record for the API
-            token_record = self.env['mtd.api_tokens'].search([('api_id', '=', self.api_name.id)])
-            self._logger.info(
-                "Connection button Clicked - endpoint name {} , ".format(self.name) +
-                "and the api is :- {} ".format(self.api_name)
-            )
-            if not token_record.access_token and not token_record.refresh_token:
-                self._logger.info(
-                    "Connection button Clicked - endpoint name {0}, No access token ".format(self.name) +
-                    "found and no refresh_token found from the token record table."
-                )
-                api_tracker = self.env['mtd.api_request_tracker'].search([
-                    ('request_sent', '=', True),
-                    ('response_received', '=', False)
-                ])
-                self._logger.info(
-                    "Connection button Clicked - endpoint name {}, ".format(self.name) +
-                    "Checking to see if a request is in process"
-                )
-                time_10_mins_ago = (datetime.now() - timedelta(minutes=10))
-                format_time_10_mins_ago = time_10_mins_ago.strftime('%Y-%m-%d- %H:%M:%S')
-                if api_tracker:
-                    if format_time_10_mins_ago >= api_tracker.create_date:
-                        # this means that time is more than 10 minutes a go so we can place a new request
-                        # and close this request.
-                        api_tracker.response_received = True
-                        self._logger.info(
-                            "Connection button Clicked - endpoint name {}, ".format(self.name) +
-                            "no Pending requests"
-                        )
-                        return self.get_user_authorisation()
-                    else:
-                        # The request made was within 10 mins so the user has to wait.
-                        raise exceptions.Warning(
-                            "An authorisation request is already in process!!!\n " +
-                            "Please try again later"
-                        )
-                else:
+            time_10_mins_ago = (datetime.now() - timedelta(minutes=10))
+            format_time_10_mins_ago = time_10_mins_ago.strftime('%Y-%m-%d- %H:%M:%S')
+            if api_tracker:
+                if format_time_10_mins_ago >= api_tracker.create_date:
+                    # this means that time is more than 10 minutes a go so we can place a new request
+                    # and close this request.
+                    api_tracker.response_received = True
+                    self._logger.info(
+                        "Connection button Clicked - endpoint name {}, ".format(self.name) +
+                        "no Pending requests"
+                    )
                     return self.get_user_authorisation()
+                else:
+                    # The request made was within 10 mins so the user has to wait.
+                    raise exceptions.Warning(
+                        "An authorisation request is already in process!!!\n " +
+                        "Please try again later"
+                    )
             else:
-                self._logger.info(
-                    "Connection button Clicked - endpoint name {}, ".format(self.name) +
-                    "We have access token and refresh token"
-                )
-                version = self._json_command('version')
-                return version
+                return self.get_user_authorisation()
         else:
-            raise exceptions.Warning("Could not connect to HMRC! \nThis is not a valid end point")
+            self._logger.info(
+                "Connection button Clicked - endpoint name {}, ".format(self.name) +
+                "We have access token and refresh token"
+            )
+            version = self._json_command('version')
+            return version
     
     def _json_command(self, command, timeout=3, record_id=None):
         # this will determine where we have come from
