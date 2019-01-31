5b36d3389ed6bdccf59a2e715a637d0633086556
Shree Sangani <shree.sangani@opusvl.com>
2019-01-31 14:53:58 +0000

    [IMP][O1758] chanign variable name to new name
    adding an extra selection option for the variable

--- account_mtd/models/mtd_api_request_tracker.py
+++ account_mtd/models/mtd_api_request_tracker.py
@@ -20,6 +20,7 @@ class MtdApiRequestTracker(models.Model):
     closed = fields.Selection([
         ('timed_out', 'Request timed out'),
         ('response', 'Response received'),
+        ('more_than_one', 'More than one open tracker found')
     ], string='Tracker Closed')
     action = fields.Char(required=True)
     menu_id = fields.Char(required=True)
