717b08055826d56dff9d4cad90fe9529e58b5864
Shree Sangani <shree.sangani@opusvl.com>
2019-02-18 16:46:03 +0000

    [IMP][VAT-V8] capturing response change
    As the response may return error_description or message

--- account_mtd/models/mtd_construct_display_message.py
+++ account_mtd/models/mtd_construct_display_message.py
@@ -8,9 +8,18 @@ class MtdUserAuthorisation(models.Model):
     _name = 'mtd.display_message'
     _description = "construct error message to doisplay to the user"
 
-    def consturct_error_message_to_display(self, url=None, code=None, message=None, error=None):
-        if error:
-            error = "\n{}".format(error)
+    def consturct_error_message_to_display(self, url=None, code=None, response_token=None):
+        resp_message=''
+        resp_error=''
+        if 'error_description' in response_token.keys():
+            resp_message = response_token['error_description']
+        elif 'message' in response_token.keys():
+            resp_message = response_token['message']
+        if 'error' in response_token.keys():
+            resp_error = response_token['error']
+        if resp_error:
+            resp_error = "\n{}".format(resp_error)
+
         error_message = (
                 "Date {date}     Time {time} \n".format(date=datetime.now().date(), time=datetime.now().time())
                 + "Sorry. The connection failed ! \n"
