205a5d45b0401ea96f435198c73622c9db6f3ff5
Shree Sangani <shree.sangani@opusvl.com>
2019-02-26 15:56:37 +0000

    [IMP][O1851] Adding error message for response
    Adding the error message as was not beign captured.

--- account_mtd/models/mtd_construct_display_message.py
+++ account_mtd/models/mtd_construct_display_message.py
@@ -17,6 +17,15 @@ class MtdUserAuthorisation(models.Model):
             resp_message = response_token['message']
         if 'error' in response_token.keys():
             resp_error = response_token['error']
+        resp_error_message = ""
+        if 'errors' in response_token.keys():
+            respo_errors = response_token['errors']
+            for error in respo_errors:
+                error_code = error["code"]
+                error_message = error["message"]
+
+                resp_error_message += "\n\nError Code: \n{}\nError Message: \n{}".format(error_code, error_message)
+
         if resp_error:
             resp_error = "\n{}".format(resp_error)
 
@@ -27,7 +36,10 @@ class MtdUserAuthorisation(models.Model):
                 + "Connection Status Details: \n"
                 + "Request Sent: \n{auth_url} \n\n".format(auth_url=url)
                 + "Error Code:\n{code} \n\n".format(code=code)
-                + "Response Received: {error}\n{message}".format(error=resp_error, message=resp_message)
-        )
+                + "Response Received: {resp_error}\n{message}{resp_error_message}".format(
+            resp_error=resp_error,
+            message=resp_message,
+            resp_error_message=resp_error_message
+        ))
 
         return error_message
