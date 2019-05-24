27db47edbf8df4d33420f57792ae3f519526c009
Shree Sangani <shree.sangani@opusvl.com>
2019-05-24 10:18:54 +0100

    [IMP][MTD VAT V10] updating Version Number

--- account_mtd/__manifest__.py
+++ account_mtd/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'UK HMRC MTD - Connector',
-    'version': '0.1',
+    'version': '0.2',
     'author': 'OpusVL',
     'website': 'http://opusvl.com/',
     'summary': '',
--- account_mtd_vat/__manifest__.py
+++ account_mtd_vat/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'UK HMRC MTD- VAT',
-    'version': '0.1',
+    'version': '0.2',
     'author': 'OpusVL',
     'website': 'http://opusvl.com/',
     'summary': '',
