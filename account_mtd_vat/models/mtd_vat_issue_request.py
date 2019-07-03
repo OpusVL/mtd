1bfe051f933f14b7e3ed811236fc3ae30ef7f9c2
Nick Booker <nick.booker@opusvl.com>
2019-07-03 14:15:09 +0100

    [REF] Remove extra blank line
    pep-8 says sparing blank lines within method, and I don't think this one was adding
    anything.

--- account_mtd_vat/models/mtd_vat_issue_request.py
+++ account_mtd_vat/models/mtd_vat_issue_request.py
@@ -616,7 +616,6 @@ class MtdVatIssueRequest(models.Model):
         # active_ids are the ones that would have been ticked in the GUI
         context['active_ids'] = move_line_account_id
         context['reconciliation_allowed_on_all_accounts'] = True
-
         account_move_line_obj.reconcile(self._cr, self._uid, move_line_account_id, 'manual', account_id, period_id, journal_id, context=context)
 
 
