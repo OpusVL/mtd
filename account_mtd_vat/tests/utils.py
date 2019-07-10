85341820848c17a2ca0c2f24fb09f0b319713adc
Armand Cela <armand.cela@opusvl.com>
2019-07-10 15:29:39 +0100

    [O2010] Move test helper function into utils (Commits in OpusVL/odoo-account-extras 8.0)

--- account_mtd_vat/tests/utils.py
+++ account_mtd_vat/tests/utils.py
@@ -3,3 +3,7 @@ def arbitrary(iterable):
         return next(iter(iterable))
     except StopIteration:
         raise IndexError("Iterable is empty")
+
+
+def all_records_in_model(odoo_model):
+    return odoo_model.search([])
