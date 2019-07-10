ead05c1c9970d5efd89236d184380433c0b89b3e
Armand Cela <armand.cela@opusvl.com>
2019-07-10 17:25:20 +0100

    [O2010] Fix spelling error in error message (Commits in OpusVL/odoo-account-extras 8.0)

--- account_mtd_vat/models/mtd_vat_endpoint.py
+++ account_mtd_vat/models/mtd_vat_endpoint.py
@@ -403,7 +403,7 @@ class MtdVATEndpoints(models.Model):
         if not hmrc_posting_config:
             raise exceptions.Warning(
                 "Chart of Taxes can not be generated!\n " +
-                "Please create HMRC Posting Templae record first"
+                "Please create HMRC Posting Template record first "
             )
         vrn = self.get_vrn(self.vrn)
         period_key = urllib.quote_plus(self.select_vat_obligation.period_key)
