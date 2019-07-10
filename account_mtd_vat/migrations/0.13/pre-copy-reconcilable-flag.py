79a7cde2110ef3eb37651bc6c391463bc065e9b3
Armand Cela <armand.cela@opusvl.com>
2019-07-10 17:20:23 +0100

    [O2010] Also encapsulated the column_exists logic into a function.(Commits in OpusVL/odoo-account-extras 8.0)

--- account_mtd_vat/migrations/0.13/pre-copy-reconcilable-flag.py
+++ account_mtd_vat/migrations/0.13/pre-copy-reconcilable-flag.py
@@ -1,21 +1,23 @@
 def migrate(cr, installed_version):
-    cr.execute("""
-            select exists (
-                SELECT 1
-                FROM information_schema.columns
-                WHERE
-                    table_schema='public'
-                    AND table_name='account_account'
-                    AND column_name='not_reconcilable_by_user'
-            )
-        """)
-    (column_already_exists,) = cr.fetchone()
-    if not column_already_exists:
+    if not column_exists(cr, schema='public', table='account_account',
+            column='not_reconcilable_by_user'):
         cr.execute("""
             ALTER TABLE account_account
-            ADD COLUMN not_reconcilable_by_user BOOLEAN 
+            RENAME COLUMN reconcile TO not_reconcilable_by_user 
         """)
-    cr.execute("""
-            UPDATE account_account
-                SET not_reconcilable_by_user = reconcile
-        """)
+
+
+def column_exists(cr, schema, table, column):
+    sql = """
+        select exists (
+            SELECT 1
+            FROM information_schema.columns
+            WHERE
+                table_schema=%s
+                AND table_name=%s
+                AND column_name=%s
+        )
+    """
+    cr.execute(sql, (schema, table, column,))
+    (exists,) = cr.fetchone()
+    return exists
