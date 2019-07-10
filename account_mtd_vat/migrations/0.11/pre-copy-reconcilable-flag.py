33eac8c8bb48703d06ccf40ec770b9b467a2a9af
Armand Cela <armand.cela@opusvl.com>
2019-07-10 15:01:04 +0100

    [O2010] Copy backend field so we can compute the frontend one, Override reconcile flag according to flag in context (Commits in OpusVL/odoo-account-extras)

---
+++ account_mtd_vat/migrations/0.11/pre-copy-reconcilable-flag.py
@@ -0,0 +1,21 @@
+def migrate(cr, installed_version):
+    cr.execute("""
+            select exists (
+                SELECT 1
+                FROM information_schema.columns
+                WHERE
+                    table_schema='public'
+                    AND table_name='account_account'
+                    AND column_name='non_mtd_reconcilable'
+            )
+        """)
+    (column_already_exists,) = cr.fetchone()
+    if not column_already_exists:
+        cr.execute("""
+            ALTER TABLE account_account
+            ADD COLUMN non_mtd_reconcilable BOOLEAN 
+        """)
+    cr.execute("""
+            UPDATE account_account
+                SET non_mtd_reconcilable = reconcile
+        """)
