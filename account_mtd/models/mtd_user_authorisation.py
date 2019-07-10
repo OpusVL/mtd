823b41720cd28e435fcc30d3bcffe5ac396b2ba6
Armand Cela <armand.cela@opusvl.com>
2019-07-10 17:56:17 +0100

    [O2010] Make redirect URL field tolerant of trailing / appended by user (Commits in OpusVL/odoo-account-extras 8.0)

--- account_mtd/models/mtd_user_authorisation.py
+++ account_mtd/models/mtd_user_authorisation.py
@@ -19,7 +19,8 @@ class MtdUserAuthorisation(models.Model):
     @api.multi
     def get_user_authorisation(self, module_name=None, record=None):
         tracker_api = self.create_tracker_record(module_name, record)
-        redirect_uri = "{}/auth-redirect".format(record.hmrc_configuration.redirect_url)
+        normalised_redirect_url = record.hmrc_configuration.redirect_url
+        redirect_uri = "{}/auth-redirect".format(normalised_redirect_url)
         state = ""
         # State is optional
 
