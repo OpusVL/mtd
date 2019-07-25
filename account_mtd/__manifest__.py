dad2050c407d90b0cd74923e95aac98b4d263ffc
Shree Sangani <shree.sangani@opusvl.com>
2019-07-25 16:26:26 +0100

    Bump version no to 0.8

--- account_mtd/__manifest__.py
+++ account_mtd/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'UK HMRC MTD - Connector',
-    'version': '0.7',
+    'version': '0.8',
     'author': 'OpusVL',
     'website': 'https://opusvl.com/',
     'summary': 'This module enables Odoo Community and Enterprise to establish an authenticated connection to the HMRC’s Making Tax Digital platform.',
