bd2507e9c42601387c94226025817da0ffc81644
Armand Cela <armand.cela@opusvl.com>
2019-09-12 09:39:00 +0100

    [O2058] Removing computed field for allowing reconcilation

--- account_mtd_vat/tests/test_accont_account.py
+++ account_mtd_vat/tests/test_accont_account.py
@@ -7,34 +7,15 @@ class GivenBothReconcilableAndNonReconcilableAccounts_Tests(
     def setUp(self):
         super(GivenBothReconcilableAndNonReconcilableAccounts_Tests, self).setUp()
         self.Account = self.env['account.account']
-        self._make_us_have_at_least_one_reconcilable_and_unreconcilable_account()
 
-    def _make_us_have_at_least_one_reconcilable_and_unreconcilable_account(self):
-        all_accounts = all_records_in_model(self.Account)
-        reconcilable = arbitrary_from(all_accounts)
-        reconcilable.not_reconcilable_by_user = True
-        nonreconcilable = arbitrary_from(all_accounts - reconcilable)
-        nonreconcilable.not_reconcilable_by_user = False
-
-    def test_search_for_reconcile_True(self):
-        result = self.Account.search([('reconcile', '=', True)])
-        self.assertTrue(all(a.not_reconcilable_by_user for a in result),
-            "All matches must be not_reconcilable_by_user")
-
-    def test_search_for_reconcile_False(self):
-        result = self.Account.search([('reconcile', '=', False)])
-        self.assertTrue(all((not a.not_reconcilable_by_user) for a in result),
-            "All matches must not be not_reconcilable_by_user")
 
 class GivenAccount(common.TransactionCase):
+
+
     def setUp(self):
         super(GivenAccount, self).setUp()
         self.account = arbitrary_from(
             all_records_in_model(self.env['account.account']))
-        self.account.not_reconcilable_by_user = self.not_reconcilable_by_user()
-
-    def initial_not_reconcilable_by_user(self):
-        self.skipTest("ABSTRACT: initial_not_reconcilable_by_user")
 
     def test_reconcile_True_with_context(self):
         self.assertTrue(
@@ -43,32 +24,13 @@ class GivenAccount(common.TransactionCase):
             .reconcile
         )
 
-
-    def test_set_core_reconcile_flag_True_copies_to_not_reconcilable_by_user(self):
-        self.account.reconcile = True
-        self.assertTrue(self.account.not_reconcilable_by_user,
-            "copied to not_reconcilable_by_user")
-        self.assertTrue(self.account.reconcile,
-            "reflected in computed reconcile flag")
-
-    def test_set_core_reconcile_flag_False_copies_to_not_reconcilable_by_user(self):
-        self.account.reconcile = False
-        self.assertFalse(self.account.not_reconcilable_by_user,
-            "copied to not_reconcilable_by_user")
-        self.assertFalse(self.account.reconcile,
-            "reflected in computed reconcile flag")
-
 class GivenNonUserReconcilableAccount_Tests(GivenAccount):
-    def initial_not_reconcilable_by_user(self):
-        return False
 
     def test_reconcile_False_without_context(self):
         self.assertFalse(self.account.reconcile)
 
 
 class GivenUserReconcilableAccount_Tests(GivenAccount):
-    def initial_not_reconcilable_by_user(self):
-        return True
 
     def test_reconcile_True_without_context(self):
         self.assertTrue(self.account.reconcile)
