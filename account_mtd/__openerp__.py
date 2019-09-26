40ee6c6f1452dc75ad7fc0e65fbdfea105a017a7
Armand Cela <armand.cela@opusvl.com>
2019-09-26 11:57:59 +0100

    Updating version number

--- account_mtd/__openerp__.py
+++ account_mtd/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'UK HMRC MTD - Connector',
-    'version': '0.15',
+    'version': '0.16',
     'author': 'OpusVL',
     'website': 'https://opusvl.com/',
     'summary': 'This module enables Odoo Community and Enterprise to establish an authenticated connection to the HMRC’s Making Tax Digital platform.',
--- account_mtd_vat/__openerp__.py
+++ account_mtd_vat/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'UK HMRC MTD - VAT',
-    'version': '0.15',
+    'version': '0.16',
     'author': 'OpusVL',
     'website': 'https://opusvl.com/',
     'summary': 'This module enables the collation and transmission of VAT data from Odoo to HMRC in line with Making Tax Digital requirements. From your sales and purchases in Odoo, your VAT liability is automatically calculated and submitted to HMRC.',
