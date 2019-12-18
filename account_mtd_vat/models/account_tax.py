f089667d703e4e13ad32110da6f0a65e2a7a3c48
Armand Cela <armand.cela@opusvl.com>
2019-12-18 11:38:25 +0000

    [O2286] replacing tag_ids with description, cause tag_ids doesnt egsist in v13

--- account_mtd_vat/models/account_tax.py
+++ account_mtd_vat/models/account_tax.py
@@ -18,12 +18,12 @@ class MtdAccountTaxCode(osv.osv):
 
     def _update_vat_tax_scope(self):
         for record in self:
-            if record.description== 'PT8M':
+            if record.description == 'PT8M':
                 record.vat_tax_scope = 'PTM'
             elif record.description == 'PT8R':
                 record.vat_tax_scope = 'PTR'
-            elif record.description.startswith('ST'): # in ('ST0', 'ST1', 'ST11', 'ST2', 'ST4'):
+            elif record.description.startswith('ST'):  # in ('ST0', 'ST1', 'ST11', 'ST2', 'ST4'):
                 record.vat_tax_scope = 'ST'
-            elif record.description.startswith('PT'): #  in ('PT0', 'PT1', 'PT11', 'PT2', 'PT5', 'PT7', 'PT8'):
+            elif record.description.startswith('PT'):  # in ('PT0', 'PT1', 'PT11', 'PT2', 'PT5', 'PT7', 'PT8'):
                 record.vat_tax_scope = 'PT'
         return True
