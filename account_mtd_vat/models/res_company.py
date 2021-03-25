1f35d4c744301c72464b0d25171c292a89250f31
Nick Booker <nick.booker@opusvl.com>
2021-03-25 13:19:30 +0000

    Temporary fix to allow upgrade past removal of hmrc_username field
    Get the patched res_company.py in (e.g. via a temporary docker image),
    upgrade account_mtd once with this image, then go back to the image you
    intended and upgrade it again.

--- account_mtd_vat/models/res_company.py
+++ account_mtd_vat/models/res_company.py
@@ -6,3 +6,7 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     hmrc_posting_created = fields.Boolean(default=False)
+
+    # <TEMPORARY to let the upgrades work>
+    hmrc_username = fields.Char()
+    # </TEMPORARY>
